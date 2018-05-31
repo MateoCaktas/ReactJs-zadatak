f1733131d3a02aaf120224750120e99729e2a10b
Mateo Caktaš <mcakta00@fesb.hr>
2018-05-31 16:04:44 +0200

    Drugi commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <MyComponent />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -18,4 +18,24 @@ class App extends Component {
   }
 }
 
+class MyComponent extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {isToggleOn: true};
+    
+    this.buttonClicked = this.buttonClicked.bind(this);
+  }
+  buttonClicked(){
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+  render(){
+    return(     
+      <button onClick={this.buttonClicked}>
+       {this.state.isToggleOn ? 'Crna' : 'Random'}
+        </button>
+    );
+  }
+}
 export default App;
